0f77273bce6c90be6340a8eeab3d5690d559a447
psahithy <parnapalli.sahithya@capgemini.com>
2024-09-16 11:43:58 +0530

    threads practice first commit

---
+++ src/main/java/com/example/demo/A.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class A extends Thread {
+	
+	public void run()
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("thread A");
+//			try {
+//				Thread.sleep(10);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		}
+	}
+	public void show()
+	{
+		System.out.println("show");
+	}
+	
+
+}
